7056dfd770282449d71123c6561fa439b7f11fb3
eugenius123 <yugene123@gmail.com>
2019-01-23 18:19:04 -0500

    initial commit with bootstrap

---
+++ app/javascript/packs/dashboard.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react' 
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className = "container">
+        <header className = "text-center">
+        <h1 className = "page-header">React Fit</h1>
+        <p className = "lead">Your personal fitness dashboard</p>
+        </header>
+      </div>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Dashboard />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
+
